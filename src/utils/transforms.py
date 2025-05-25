6f47e8fa83e9577779faa959bab5bcc053a653af
Marcel Bieniek <marcel.bieniek@gmail.com>
2025-05-25 21:47:27 +0000

    config and transforms updates

--- src/utils/transforms.py
+++ src/utils/transforms.py
@@ -6,7 +6,7 @@ classification_image_tf = transforms.Compose([
     transforms.Resize((299, 299)),
     transforms.RandomHorizontalFlip(0.5), # data augmentation
     transforms.RandomVerticalFlip(0.5), # data augmentation
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2), # data augmentation
+    # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2), # data augmentation
     transforms.ToTensor(),
     # transforms.Normalize(mean=[0.485, 0.456, 0.406],  # Normalize with ImageNet mean and std
     #                      std=[0.229, 0.224, 0.225])
@@ -14,20 +14,21 @@ classification_image_tf = transforms.Compose([
 
 segmentation_image_tf = transforms.Compose([
     transforms.Resize((299, 299)),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2), # data augmentation
+    # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2), # data augmentation
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],  # Normalize with ImageNet mean and std
                          std=[0.229, 0.224, 0.225])
 ])
 
 def segmentation_mask_tf(mask):
-    mask = mask.resize((299, 299), resample=Image.BILINEAR)
+    mask = mask.resize((299, 299), resample=Image.BILINEAR) # data augmentation
     # mask = torch.from_numpy(np.array(mask)).long()  # shape [H, W], values 0-9
     mask = TF.pil_to_tensor(mask).long()
     return mask
 
 # data augmentation
 seg_joint_transform = transforms.Compose([
+    transforms.Resize((400, 300)),
     transforms.RandomCrop((299,299)),
     transforms.RandomHorizontalFlip(0.5),
     transforms.RandomVerticalFlip(0.5),
